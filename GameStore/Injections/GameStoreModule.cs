11b7312e33765028bde503059c241621baa91276
HriGunov <hristo.d.gunov@gmail.com>
2018-10-08 10:27:28 +0300

    Created RegisterCommands functionality in Injection logic.

--- GameStore/Injections/GameStoreModule.cs
+++ GameStore/Injections/GameStoreModule.cs
@@ -4,10 +4,13 @@ using GameStore.Core;
 using GameStore.Data.Context;
 using GameStore.Services;
 using GameStore.Services.Abstract;
+using System;
+using System.Linq;
+using System.Reflection;
 
 namespace GameStore.Injections
 {
-    public class GameStoreModule : Module
+    public class GameStoreModule : Autofac.Module
     {
         protected override void Load(ContainerBuilder builder)
         {
@@ -17,8 +20,20 @@ namespace GameStore.Injections
             builder.RegisterType<ProductsService>().As<IProductsService>();
             builder.RegisterType<ShoppingCartsService>().As<IShoppingCartsService>();
             builder.RegisterType<CommandManager>().As<ICommandManager>();
-
+            RegisterCommands(builder);
             base.Load(builder);
+        }
+
+        private void RegisterCommands(ContainerBuilder builder)
+        {
+            var type = typeof(ICommand);
+            var commands = AppDomain.CurrentDomain.GetAssemblies().SelectMany(s => s.GetTypes()).Where(p => type.IsAssignableFrom(p) && !p.IsAbstract &&!p.IsInterface);
+
+            foreach (var command in commands)
+            {
+                builder.RegisterType(command).Named<ICommand>(command.Name.ToLower()).SingleInstance();
+            }
+            
         }
     }
 }
