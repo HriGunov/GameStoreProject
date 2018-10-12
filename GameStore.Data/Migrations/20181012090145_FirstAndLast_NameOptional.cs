c361b6c628f688c36f259643820983dfd7153ac9
eXtraGoZ <stranged96@gmail.com>
2018-10-12 12:02:58 +0300

    Migration (Optional FirstName & LastName)

---
+++ GameStore.Data/Migrations/20181012090145_FirstAndLast_NameOptional.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GameStore.Data.Migrations
+{
+    public partial class FirstAndLast_NameOptional : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Accounts",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Accounts",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 20);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Accounts",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 20,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Accounts",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 20,
+                oldNullable: true);
+        }
+    }
+}
