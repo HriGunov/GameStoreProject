0531d3e5db27291a56885a23541bef25bb7c4351
eXtraGoZ <stranged96@gmail.com>
2018-10-12 12:50:32 +0300

    Configuration Split

---
+++ GameStore.Data/Context/Configurations/GenreConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using GameStore.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GameStore.Data.Context.Configurations
+{
+    internal class GenreConfiguration : IEntityTypeConfiguration<Genre>
+    {
+        public void Configure(EntityTypeBuilder<Genre> builder)
+        {
+            #region Genre Setup
+            builder.ToTable("Genres");
+
+            builder.Property(g => g.Name)
+                        .IsRequired(true);
+            #endregion
+        }
+    }
+}
---
+++ GameStore.Data/Context/Configurations/ProductConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using GameStore.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GameStore.Data.Context.Configurations
+{
+    internal class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            #region Product Setup
+            builder.ToTable("Products");
+
+            builder.HasIndex(p => p.Name)
+                .IsUnique(true);
+
+            builder.Property(p => p.Name)
+                        .HasMaxLength(20)
+                        .IsRequired(true);
+
+            builder.Property(p => p.Description)
+                        .HasMaxLength(100)
+                        .IsRequired(true);
+
+            builder.Property(p => p.Price)
+                        .IsRequired(true);
+            #endregion
+        }
+    }
+}
