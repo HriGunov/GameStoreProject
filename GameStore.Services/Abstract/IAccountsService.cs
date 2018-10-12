ca8ffd149091da0663547b6a9eeddacb2ab0c4ed
eXtraGoZ <stranged96@gmail.com>
2018-10-12 13:15:17 +0300

    Interface Update

--- GameStore.Services/Abstract/IAccountsService.cs
+++ GameStore.Services/Abstract/IAccountsService.cs
@@ -4,13 +4,12 @@ namespace GameStore.Services.Abstract
 {
     public interface IAccountsService
     {
-        Account AddAccount(string firstName, string lastName, string userName, string password, bool isAdmin = false,
-            bool isGuest = false);
+        Account AddAccount(string firstName, string lastName, string userName, string password, bool isAdmin = false, bool isGuest = false);
         Account AddAccount(Account account);
-
+        Account FindAccount(string accountName);
+        Account GetGuestAccount();
         bool IsAdmin(string accountName);
         string RemoveAccount(string commandExecutor, string accountName);
-
-        Account FindAccount(string accountName);
+        string RestoreAccount(string commandExecutor, string accountName);
     }
 }
