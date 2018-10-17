1c94a37f035fd7c963d7d4720aba2611bc721e22
HriGunov <hristo.d.gunov@gmail.com>
2018-10-17 03:50:36 +0300

    Fixed Help Command

--- GameStore/Commands/HelpCommand.cs
+++ GameStore/Commands/HelpCommand.cs
@@ -1,21 +1,21 @@
-﻿using System;
+﻿using GameStore.Commands.Abstract;
+using GameStore.Core.Abstract;
+using System;
 using System.Collections.Generic;
-using GameStore.Commands.Abstract;
 
 namespace GameStore.Commands
 {
     internal class HelpCommand : ICommand
     {
-        private readonly ICommandManager commandManager;
-
-        public HelpCommand(ICommandManager commandManager)
+         
+        public HelpCommand()
         {
-            this.commandManager = commandManager;
+             
         }
 
         public string Execute(List<string> parameters)
         {
-            throw new NotImplementedException();
+            return "Ask a developer for help. :D";
         }
     }
 }
