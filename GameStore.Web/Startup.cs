97e27cb7fdce1f263365939717cb1280ffb1b1f5
HriGunov <hristo.d.gunov@gmail.com>
2018-11-01 12:06:38 +0200

    Added Empty Admin Area

--- GameStore.Web/Startup.cs
+++ GameStore.Web/Startup.cs
@@ -93,6 +93,11 @@ namespace GameStore.Web
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
+                      name: "areas",
+                      template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                    );
+
+                routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
