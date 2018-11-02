69dc63b55eb5085e4643f5c8dd9142ca4573b9b0
eXtraGoZ <stranged96@gmail.com>
2018-11-02 10:34:29 +0200

    Bootstrap 4 Files & Areas & Routing

---
+++ GameStore.Web/Areas/Administration/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using GameStore.Services.Abstract;
+using GameStore.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameStore.Web.Areas.Administration.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+            
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
