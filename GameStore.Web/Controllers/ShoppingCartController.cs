017a6b92c218dc45395a1ab3467126187e42c453
HriGunov <hristo.d.gunov@gmail.com>
2018-11-04 20:10:54 +0200

    Added Cart (Work in Progress)

---
+++ GameStore.Web/Controllers/ShoppingCartController.cs
@@ -0,0 +1,27 @@
+﻿using GameStore.Data.Models;
+using GameStore.Services.Abstract;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace GameStore.Web.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        private readonly IShoppingCartsService _shoppingCartsService;
+        private readonly UserManager<Account> userManager;
+
+        public ShoppingCartController(UserManager<Account> userManager, IShoppingCartsService shoppingCartsService)
+        {
+            this._shoppingCartsService = shoppingCartsService;
+            this.userManager = userManager;
+        }
+        public async  Task<IActionResult> Index()
+        {
+            var userId =  userManager.GetUserId(this.User);
+            var cart = await _shoppingCartsService.GetUserCart(userId);
+                
+            return View();
+        }
+    }
+}
