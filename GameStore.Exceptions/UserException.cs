af2fb9291305ec3475dbe9dd3b620ab6ac51942d
eXtraGoZ <stranged96@gmail.com>
2018-11-12 09:22:18 +0200

    Exclude from Coverage

--- GameStore.Exceptions/UserException.cs
+++ GameStore.Exceptions/UserException.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace GameStore.Exceptions
 {
+    [ExcludeFromCodeCoverage]
     public class UserException : Exception
     {
         public UserException(string message) : base(message)
